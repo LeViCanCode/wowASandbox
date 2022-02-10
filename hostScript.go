05239cc294c6dd6dfa111756824d8d751f5a0f4f
LeViCanCode <Escamillajoey@gmail.com>
2022-02-09 16:31:14 -0800

    error fixes

--- hostScript.go
+++ hostScript.go
@@ -1,46 +1,49 @@
+package testBox
+
 //check if file is ran in a terminal https://github.com/mattn/go-isatty
 import (
 	"fmt"
-	"runtime"
 	"os"
-	"flag"
-	"strings"
+	"runtime"
+
 	"github.com/mattn/go-isatty"
 )
+
 var exists bool
 
 func windows() {
 	userDir, _ := os.UserHomeDir()
-	if _, err := os.Stat(fmt.Sprintf("%s\\VirtualBox VMs", userDir)) !os.IsNotExist(err) {
-        exists = true
+	if _, err := os.Stat(fmt.Sprintf("C:\\%s\\VirtualBox VMs", userDir)); !os.IsNotExist(err) {
+		exists = true
+		fmt.Println(exists)
 	} else {
 		fmt.Println("Please specify a path to wher your VM's are saved")
-		os.Exit()
+		os.Exit(0)
 	}
 }
 func linux() {
 	userDir, _ := os.UserHomeDir()
-	if _, err := os.Stat(fmt.Sprintf("%s/VirtualBox VMs", userDir) !os.IsNotExist(err) {
-        exists = true
+	if _, err := os.Stat(fmt.Sprintf("%s/VirtualBox VMs", userDir)); !os.IsNotExist(err) {
+		exists = true
 	} else {
 		fmt.Println("Please specify a path to wher your VM's are saved")
-		os.Exit()
+		os.Exit(0)
 	}
 }
-func main() {
+func init() {
 	if isatty.IsTerminal(os.Stdout.Fd()) {
 		fmt.Println("Is Terminal")
 	} else if isatty.IsCygwinTerminal(os.Stdout.Fd()) {
 		fmt.Println("Is Cygwin/MSYS2 Terminal")
 	} else {
-		os.Exit()
+		os.Exit(0)
 	}
-	newPath := flag.String("path")
-	flag.Parse()
-	if !strings.Matches(*newPath, /[/]+/g) {
-		fmt.Println("Please include the full file path.")
-		os.Exit()
-	}
+	// newPath := flag.String("path")
+	// flag.Parse()
+	// if !strings.Matches(*newPath, /[/]+/g) {
+	// 	fmt.Println("Please include the full file path.")
+	// 	os.Exit()
+	// }
 	if runtime.GOOS == "windows" {
 		windows()
 	} else {
