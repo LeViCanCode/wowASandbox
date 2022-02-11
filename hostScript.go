01e754ca90d225e082c40365149b9392d60a679b
LeViCanCode <Escamillajoey@gmail.com>
2022-02-10 16:30:57 -0800

    fix import issue

--- hostScript.go
+++ hostScript.go
@@ -5,27 +5,46 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"os/exec"
 	"runtime"
 
+	"github.com/klauspost/cpuid/v2"
 	"github.com/mattn/go-isatty"
 )
 
 func windows() {
 	userDir, _ := os.UserHomeDir()
+	name := cpuid.CPU.BrandName
+	physCores := cpuid.CPU.PhysicalCores
+	threadsPC := cpuid.CPU.ThreadsPerCore
+	logicCores := cpuid.CPU.LogicalCores
+	family := cpuid.CPU.Family
+	model := cpuid.CPU.Model
+	vendorID := cpuid.CPU.VendorID
+	vm := cpuid.CPU.VM()
+	fmt.Printf(name, "\n", physCores, "\n", threadsPC, "\n", logicCores, "\n", family, "\n", model, "\n", vendorID)
+	if vm {
+		fmt.Println("cpu id not spoofed")
+	}
+
 	if _, err := os.Stat(fmt.Sprintf("%s\\VirtualBox VMs", userDir)); !errors.Is(err, os.ErrNotExist) {
 		fmt.Println("exists")
-		os.Exit(0)
+		list := exec.Command("vboxmanage", "list").Run()
+		fmt.Println(list)
+		// os.Exit(0)
 
 	} else {
-		fmt.Println("Please specify a path to wher your VM's are saved")
+		fmt.Println("Please specify a path to where your VMs are saved")
 		os.Exit(0)
 	}
 }
 func linux() {
 	userDir, _ := os.UserHomeDir()
-	if _, err := os.Stat(fmt.Sprintf("%s/VirtualBox VMs", userDir)); !os.IsNotExist(err) {
+	if _, err := os.Stat(fmt.Sprintf("%s\\VirtualBox VMs", userDir)); !errors.Is(err, os.ErrNotExist) {
+		fmt.Println("exists")
+		os.Exit(0)
 	} else {
-		fmt.Println("Please specify a path to wher your VM's are saved")
+		fmt.Println("Please specify a path to where your VMs are saved")
 		os.Exit(0)
 	}
 }
@@ -34,16 +53,12 @@ func main() {
 		fmt.Println("Is Terminal")
 	} else if isatty.IsCygwinTerminal(os.Stdout.Fd()) {
 		fmt.Println("Is Cygwin/MSYS2 Terminal")
-	} //else {
-	// os.Exit(0)
-	//ereor
-	//}
-	// newPath := flag.String("path")
-	// flag.Parse()
-	// if !strings.Matches(*newPath, /[/]+/g) {
-	// 	fmt.Println("Please include the full file path.")
-	// 	os.Exit()
-	// }
+	} else {
+		os.Exit(0)
+		//later stuff
+		// newPath := flag.String("path")
+		// flag.Parse()
+	}
 	if runtime.GOOS == "windows" {
 		windows()
 	} else {
